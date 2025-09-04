abcc069c22374f82efdc1f3f656aa7e62c034c81
Hoàng Văn Quân <phanthiquynhtrang@192.168.0.112.non-exists.ptr.local>
2025-09-04 11:09:17 +0700

    bt

---
+++ ss21/src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import Execise01 from './ex01'
+import Execise02 from './ex02'
+import Execise03 from './ex03'
+import Execise04 from './ex04'
+import Execise05 from './ex05'
+import Execise06 from './ex06'
+import Execise07 from './ex07'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <Execise01/> */}
+      {/* <Execise02/> */}
+      <Execise03/>
+      {/* <Execise04/> */}
+      {/* <Execise05/> */}
+      {/* <Execise06/> */}
+      {/* <Execise07/> */}
+    </>
+  )
+}
+
+export default App
